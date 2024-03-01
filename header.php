0a587e4622386577697b34a8b64d97a3c51d7a3a
Luca <luca@ahd-creative.agency>
2024-03-01 14:41:57 +0100

    feat: 404 page

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="with-device-width">
+    <meta name="viewport" content="width=device-width">
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php wp_head(); ?>
